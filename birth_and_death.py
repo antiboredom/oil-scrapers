aa8734dd9a10027fa3831e39be168fcd93b7064f
Sam Lavigne <splavigne@gmail.com>
2022-08-29 17:22:53 -0400

    update births and deaths

--- birth_and_death.py
+++ birth_and_death.py
@@ -2,11 +2,20 @@ from dateutil import parser
 import pandas as pd
 import numpy as np
 
-date_template = "%A %B %-d %Y"
+# date_template = "%A %B %-d %Y"
+date_template = "%A %B {S} %Y"
+
+
+def suffix(d):
+    return "th" if 11 <= d <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(d % 10, "th")
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
 
 def parse_wells():
     df = pd.read_csv("deadwells.csv")
-    df.sort_values(by=['CompletionDate'], inplace=True, ascending=True)
+    df.sort_values(by=["CompletionDate"], inplace=True, ascending=True)
     for _, row in df.iterrows():
         # try:
         #     first_prod = parser.parse(row["FirstProdDate"])
@@ -24,14 +33,16 @@ def parse_wells():
             continue
 
         # valid = completion < first_prod and first_prod < plug
-        valid = completion <  plug
+        valid = completion < plug
 
         if not valid:
             continue
 
         # first_prod = first_prod.strftime(date_template)
-        plug = plug.strftime(date_template)
-        completion = completion.strftime(date_template)
+        # plug = plug.strftime(date_template)
+        # completion = completion.strftime(date_template)
+        plug = custom_strftime(date_template, plug)
+        completion = custom_strftime(date_template, completion)
 
         name = row["Name"]
         county = row["County"]
@@ -47,7 +58,6 @@ def parse_wells():
         if np.isnan(total_gas):
             total_gas = 0
 
-
         birth = f"{name} was completed by {operator} in {location} on {completion}, "
 
         # if first_prod:
@@ -57,7 +67,7 @@ def parse_wells():
         drilling = ""
 
         death = f"and was plugged and abandoned on {plug}."
-        totals = f" It produced {total_oil} barrels of oil."
+        totals = f" It produced {total_oil:,.0f} barrels of oil."
 
         print(birth + drilling + death + totals)
 
@@ -95,5 +105,6 @@ def parse_wells():
     #     print(
     #         f"{name} was created by {operator} in {location} on {completion}, began drilling on {first_prod}, and was plugged and abandoned on {plug}"
     #     )
+
 
 parse_wells()

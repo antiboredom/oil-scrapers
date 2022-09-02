9aa85a0fe3beaeb2f638779cd73c4888bb9bfa5c
Sam Lavigne <splavigne@gmail.com>
2022-09-02 15:08:31 -0400

    update docs

--- docs/app.js
+++ docs/app.js
@@ -32,7 +32,7 @@ async function slideShow() {
   el.classList.add("content");
   el.style.padding = `${paddingV}vh ${paddingH}vh`;
   // line = line.replace("It produced", "<br><br>It produced");
-  el.innerHTML = `<small>${index + 1} of ${lines.length}</small><br><br>${line}`;
+  el.innerHTML = `<p>${index + 1} of ${lines.length}</p>${line}`;
   // el.textContent = `${line}`;
   el.style.animation = `${animation}-in ${transition}ms linear forwards`;
   container.prepend(el);
